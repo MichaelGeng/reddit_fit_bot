d734673f305c9329d2ba64dcf2d9aa8557f1936b
MichaelGeng <mikegeng@umich.edu>
2017-12-22 22:14:55 -0500

    starter code for reddit bot

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-username = "Mikegengsta"
-password = "gengsta2"
+username = "diet_recommendation"
+password = "dietisgood123"
 client_id = "hUi8YyOF_gwpKA"
 client_secret = "MNDPC715UkhrQOa9o2oTaQHUmE0"
