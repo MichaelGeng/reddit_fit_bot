295a473657c572e9b10da5ecbf409e26f22174ed
MichaelGeng <mikegeng@umich.edu>
2017-12-22 22:43:23 -0500

    Got reddit bot response working

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 username = "diet_recommendation"
 password = "dietisgood123"
-client_id = "hUi8YyOF_gwpKA"
-client_secret = "MNDPC715UkhrQOa9o2oTaQHUmE0"
+client_id = "Mjh79ApG9DXJqA"
+client_secret = "kp4JYGQ6ak_-hyrWMWIjGLFpEVQ"
--- reddit_bot.py
+++ reddit_bot.py
@@ -1,1 +1,26 @@
 import praw
+import config
+import time
+
+def bot_login():
+	r = praw.Reddit(username = config.username,
+			password = config.password,
+			client_id = config.client_id,
+			client_secret = config.client_secret,
+			user_agent = "busterronitest's dog comment responder v0.1")
+
+	return r
+
+def run_bot(r):
+	for comment in r.subreddit('test').comments(limit=25):
+		if "How to lose weight" in comment.body:
+			comment.reply("Calories in vs calories out is the important thing")
+
+	time.sleep(10)
+
+
+
+r = bot_login()
+while True:
+	run_bot(r)
+
